8658c544eb73727a628d5a4c630ae240f55c3682
ashish ghate <ashishghate5@gmail.com>
2020-09-11 22:45:45 +0530

    added header & footer

--- src/templates/SingleProduct.js
+++ src/templates/SingleProduct.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Layout from "../components/layout"
 import MobileBlock from "../components/SingleProduct/MobileBlock/MobileBlock"
 import ProductSection from "../components/SingleProduct/ProductSection/ProductSection"
 import FavoriteProducts from "../components/SingleProduct/FavoriteProducts/FavoriteProducts"
 
 const SingleProduct = (props) => {
   return (
-    <>
+    <Layout>
       <MobileBlock />
       <ProductSection productId={props['pageContext']['productId']['node']['id']}/>
       <FavoriteProducts />
-    </>
+    </Layout>
   );
 }
 
